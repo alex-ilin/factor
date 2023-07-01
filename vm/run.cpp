f6f0195c629da58163ff9989dd3603497fdd673d
Alexander Ilin <alex.ilin@protonmail.com>
2023-07-01 12:19:58 +0200

    Fix the "monotonic counter decreased" crash in VirtualBox

--- vm/run.cpp
+++ vm/run.cpp
@@ -11,13 +11,9 @@ void exit(int status) {
 
 void factor_vm::primitive_nano_count() {
   uint64_t nanos = nano_count();
-  if (nanos < last_nano_count) {
-    std::cout << "Monotonic counter decreased from 0x";
-    std::cout << std::hex << last_nano_count;
-    std::cout << " to 0x" << nanos << "." << std::dec << "\n";
-    std::cout << "Please report this error.\n";
-    current_vm()->factorbug();
-  }
+  // Work around a VirtualBox bug: http://www.virtualbox.org/ticket/6318.
+  while (nanos < last_nano_count)
+    nanos = nano_count();
   last_nano_count = nanos;
   ctx->push(from_unsigned_8(nanos));
 }
